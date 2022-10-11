942fb7a5b70231d18b63fda8a7a9db7d377ed925
walero-moon <royal.victor.leopoldino@gmail.com>
2022-10-10 18:38:41 -0700

    Rename service and create auth service

---
+++ auth_service/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def login():
+    args = request.args
+    
+    if args.get('username') == 'admin' and args.get('password') == 'admin':
+        return 'Logged in', 200
+    
+    return 'Invalid credentials', 401
+    
+if __name__ == '__main__':
+    app.run()
