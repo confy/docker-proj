4d7bb9c4c1fa6ec85d73b2a6a38d4c2d76c5c676
confy <me@adrian.ooo>
2022-10-12 17:14:58 -0700

    simple template

--- show_results/index.js
+++ show_results/index.js
@@ -8,6 +8,7 @@ import { join } from 'path';
 import fetch from 'node-fetch';
 import path from 'node:path'
 import { fileURLToPath } from 'url';
+import ejsLayouts from 'express-ejs-layouts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,6 +24,9 @@ app.use(session({
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, 'static')));
+
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
 
 // http://localhost:3000/
 app.get('/', (req, res) => {
@@ -75,7 +79,14 @@ app.post('/auth', async (req, res) => {
 app.get('/results', (req, res) => {
     if (req.session.loggedin) {
         // Render results
-        res.send("HAHAHAHAHAHAHAHA")
+        const data = [{
+            "avg_peak_hr": 120,
+            "avg_min_hr": 60,
+            "avg_cal_burned": 1000,
+            "avg_workout_duration_minutes": 60,
+            "date_calculated": "2021-10-10T00:00:00Z"
+        }]
+        res.render('results', {data: data});
     } else {
         // Render login
         res.sendFile(join(__dirname + '/static/login.html'));
